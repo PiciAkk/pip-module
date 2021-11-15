0e0d13901eb0ed9efcfd8e2f480fc0c38fa1e2fb
Székely Márton <53979153+PiciAkk@users.noreply.github.com>
2021-11-15 17:08:47 +0100

    Create pip-module.py

---
+++ pip-module.py
@@ -0,0 +1,21 @@
+import sys
+import subprocess
+
+class pip:
+    def runCommand(self, command, package):
+        process = subprocess.Popen([sys.executable, '-m', 'pip', command, package], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        exit_code = process.wait()
+        class returnableClass:
+            pass
+        returnable = returnableClass()
+        returnable.exitcode = int(exit_code)
+        returnable.stdout = stdout
+        returnable.stderr = stderr
+        return returnable
+    def telepites(self, package):
+        returnable = self.runCommand('install', package)
+        return returnable
+    def eltavolitas(self, package):
+        returnable = self.runCommand('uninstall', package)
+        return returnable
